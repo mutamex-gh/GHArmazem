76ba8084ecccd29c908a6aadc43bae6c61bb4692
Gusttavo Henrique <brgustavobarp@gmail.com>
2025-01-29 00:43:22 -0300

    only necessary

--- src/me/gharmazem/manager/InventoryButton.java
+++ src/me/gharmazem/manager/InventoryButton.java
@@ -7,19 +7,13 @@ public class InventoryButton {
 
     private String id;
     private ItemStack itemStack;
-    private Enchantment enchantment;
-    private int enchantLevel;
     private int slot;
-    private int xp;
     private double price;
 
     public InventoryButton(String id, ItemStack itemStack, int slot, double price) {
         this.id = id;
         this.itemStack = itemStack;
-        //this.enchantment = enchantment;
-        //this.enchantLevel = enchantLevel;
         this.slot = slot;
-        //this.xp = xp;
         this.price = price;
     }
 
@@ -39,22 +33,6 @@ public class InventoryButton {
         this.itemStack = itemStack;
     }
 
-    public Enchantment getEnchantment() {
-        return this.enchantment;
-    }
-
-    public void setEnchantment(Enchantment enchantment) {
-        this.enchantment = enchantment;
-    }
-
-    public int getEnchantLevel() {
-        return this.enchantLevel;
-    }
-
-    public void setEnchantLevel(int enchantLevel) {
-        this.enchantLevel = enchantLevel;
-    }
-
     public int getSlot() {
         return this.slot;
     }
@@ -63,13 +41,6 @@ public class InventoryButton {
         this.slot = slot;
     }
 
-    public int getXp() {
-        return this.xp;
-    }
-
-    public void setXp(int xp) {
-        this.xp = xp;
-    }
 
     public double getPrice() {
         return this.price;

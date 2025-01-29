6c1d21b796e881a43411a089ead39293cb85cae5
Gusttavo Henrique <brgustavobarp@gmail.com>
2025-01-29 01:10:02 -0300

    refactor update
    redone and added support for drops without fortune

--- src/me/gharmazem/listener/PSBlockBreak.java
+++ src/me/gharmazem/listener/PSBlockBreak.java
@@ -8,11 +8,13 @@ import me.gharmazem.utils.some.ActionBarUtils;
 import me.gharmazem.utils.some.ColorUtil;
 import me.gharmazem.utils.some.UtilClass;
 import org.bukkit.Material;
+import org.bukkit.NetherWartsState;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.material.NetherWarts;
 
 public class PSBlockBreak implements Listener {
 
@@ -40,28 +42,26 @@ public class PSBlockBreak implements Listener {
                             Math.floor(block.getDrops().size() * UtilClass.getFortune(player) * multiplier + 1) : 1;
 
                     if (blockMapperType != null) {
-                        BaseManager.storeSpecifyItem(player, blockMapperType, (int)dropsMultiplier);
+                        int dropsAmount = (int) dropsMultiplier;
+
+                        if (block.getType() == Material.NETHER_WARTS || block.getType() == Material.COCOA) {
+                            int baseDrops = (block.getType() == Material.NETHER_WARTS) ? 1 : 2;
+                            int fortuneMultiplier = (int) multiplier;
+                            int fortuneLevel = UtilClass.getFortune(player);
+
+                            dropsAmount = isFullyGrown ? (baseDrops + (fortuneLevel * fortuneMultiplier)) : 1;
+                        }
+                        BaseManager.storeSpecifyItem(player, blockMapperType, dropsAmount);
 
                         ActionBarUtils.sendActionBar(
                                 player,
                                 ColorUtil.colored(actionBarStore)
-                                        .replace("{quantia}", String.valueOf((int)dropsMultiplier))
+                                        .replace("{quantia}", String.valueOf(dropsAmount))
                         );
 
                         event.setCancelled(true);
                         block.setType(Material.AIR);
-                        return;
                     }
-                    BaseManager.storeSpecifyItem(player, block.getType(), (int)dropsMultiplier);
-
-                    ActionBarUtils.sendActionBar(
-                            player,
-                            ColorUtil.colored(actionBarStore)
-                                    .replace("{quantia}", String.valueOf((int)dropsMultiplier))
-                    );
-
-                    event.setCancelled(true);
-                    block.setType(Material.AIR);
                 }
             }
         }

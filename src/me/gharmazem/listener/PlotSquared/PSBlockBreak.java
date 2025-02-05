15947b5275ff27f90e40951bf918f3710e58d7a6
Gusttavo Henrique <brgustavobarp@gmail.com>
2025-02-05 00:21:01 -0300

    reformuled and added to much options

--- src/me/gharmazem/listener/PlotSquared/PSBlockBreak.java
+++ src/me/gharmazem/listener/PlotSquared/PSBlockBreak.java
@@ -2,10 +2,8 @@ package me.gharmazem.listener.PlotSquared;
 
 import com.intellectualcrafters.plot.api.PlotAPI;
 import me.gharmazem.Main;
-import me.gharmazem.manager.BaseManager;
+import me.gharmazem.manager.BonusManager;
 import me.gharmazem.manager.enums.BlockDropMapper;
-import me.gharmazem.utils.ActionBarUtils;
-import me.gharmazem.utils.ColorUtil;
 import me.gharmazem.utils.UtilClass;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -14,7 +12,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
+import java.util.List;
+
 public class PSBlockBreak implements Listener {
+
+    private final BonusManager bonusManager;
+
+    public PSBlockBreak() {
+        this.bonusManager = new BonusManager(Main.getInstance());
+    }
 
     @EventHandler
     public void onBreakBlocksInPlot(BlockBreakEvent event) {
@@ -23,56 +29,51 @@ public class PSBlockBreak implements Listener {
 
         PlotAPI plotAPI = new PlotAPI();
 
-        if (plotAPI.getPlot(block.getLocation()) == null) return;
-        if (!plotAPI.getPlot(block.getLocation()).isOwner(player.getUniqueId())) return;
-
-        String actionBarStore = Main.getInstance().getConfig().getString("Messages.actionbar-store");
         boolean isEnable = Main.getInstance().getConfig().getBoolean("PlotSquaredSupport.enable");
         boolean replantEnable = Main.getInstance().getConfig().getBoolean("PlotSquaredSupport.replant");
-        double multiplier = Main.getInstance().getConfig().getDouble("PlotSquaredSupport.multiplier");
+        List<String> toolToBreak = Main.getInstance().getConfig().getStringList("PlotSquaredSupport.tool-to-break");
 
-        if (isEnable && Main.getInstance().getAllowedItems().contains(block.getType())) {
+        if (!isEnable) return;
+        if (plotAPI.getPlot(block.getLocation()) == null) return;
+        if (!plotAPI.getPlot(block.getLocation()).isOwner(player.getUniqueId())) return;
+        if (block.getType() == Material.CACTUS) {
+            event.setCancelled(true);
+            return;
+        }
 
-            Material blockMapperType = BlockDropMapper.getDrop(block.getType());
-            boolean isFullyGrown = UtilClass.isFullyGrown(block);
-            double dropsMultiplier = isFullyGrown
-                    ? Math.floor(block.getDrops().size() + UtilClass.getFortune(player) * multiplier)
-                    : 1;
+        if (toolToBreak.contains(player.getItemInHand().getType().name())) {
+            if (Main.getInstance().getAllowedItems().contains(block.getType())) {
 
-            if (blockMapperType != null) {
-                int dropsAmount = (int) dropsMultiplier;
-                if (block.getType() == Material.NETHER_WARTS || block.getType() == Material.COCOA) {
-                    int baseDrops = (block.getType() == Material.NETHER_WARTS) ? 1 : 2;
-                    dropsAmount = isFullyGrown
-                            ? (baseDrops + (UtilClass.getFortune(player)
-                            * (int)multiplier)) : 1;
-                }
-                event.setCancelled(true);
+                Material blockMapperType = BlockDropMapper.getDrop(block.getType());
+                boolean isFullyGrown = UtilClass.isFullyGrown(block);
+                int dropsMultiplier = isFullyGrown ? block.getDrops().size() + UtilClass.getFortune(player) : 1;
 
-                if (replantEnable) {
-                    block.setType(block.getType());
-
-                    if(!isFullyGrown) {
-                        dropsAmount = 0;
+                if (blockMapperType != null) {
+                    if (block.getType() == Material.NETHER_WARTS) {
+                        int baseDrops = 1;
+                        dropsMultiplier = isFullyGrown ? baseDrops + UtilClass.getFortune(player) : 1;
                     }
-                    BaseManager.storeSpecifyItem(player, blockMapperType, dropsAmount);
-                    ActionBarUtils.sendActionBar(
-                            player,
-                            ColorUtil.colored(actionBarStore)
-                                    .replace("{quantia}", String.valueOf(dropsAmount))
-                    );
-                    return;
+                    event.setCancelled(true);
+
+                    if (replantEnable) {
+                        block.setType(block.getType());
+
+                        if (!isFullyGrown) {
+                            dropsMultiplier = 0;
+
+                            boolean isFullyGrowEnable = true;
+                            if (isFullyGrowEnable) {
+                                return;
+                            }
+                        }
+                        bonusManager.setBonus(player, blockMapperType, dropsMultiplier);
+                        return;
+                    }
+                    bonusManager.setBonus(player, blockMapperType, dropsMultiplier);
+                    block.setType(Material.AIR);
                 }
-
-                block.setType(Material.AIR);
-                BaseManager.storeSpecifyItem(player, blockMapperType, dropsAmount);
-
-                ActionBarUtils.sendActionBar(
-                        player,
-                        ColorUtil.colored(actionBarStore)
-                                .replace("{quantia}", String.valueOf(dropsAmount))
-                );
             }
         }
     }
 }
+
